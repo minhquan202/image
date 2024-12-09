0c8f1bb71954a2d88bf490ac81df6ce4408eb0aa
M_Q_uan <89106168+minhquan202@users.noreply.github.com>
2024-12-09 15:19:33 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+// Thời gian sleep tính bằng giây
+$sleepTime = 10;
+
+echo "Đang chờ $sleepTime giây...<br>";
+
+// Dừng chương trình trong $sleepTime giây
+sleep($sleepTime);
+
+echo "Đã hoàn thành!";
+?>
